7902e7291efd2f3ea1b650a92c53ed7d321ed78b
Adella <adellakenn@gmail.com>
2019-07-11 21:12:16 +0000

    Automatically backed up by Learn

---
+++ lib/board.rb
@@ -0,0 +1,17 @@
+# data and logic of a Tic-tac-toe game
+
+class Board
+  
+  def initialize
+  
+  def cells
+    
+  end # end of cells
+  
+  def reset
+    
+  end # end of reset
+  
+  
+  
+end
